39b4135ed9f1d4d097908de188e6490bb5930921
Tonghann Teng <tengtonghann@gmail.com>
2023-08-09 23:21:54 +0700

    Day 1: Two Sum

---
+++ TwoSum.kt
@@ -0,0 +1,23 @@
+package leetcode
+
+/**
+ * @author: tonghann.teng
+ * @since: 8/9/23
+ * @link: https://leetcode.com/problems/two-sum/
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = mutableMapOf<Int, Int>()
+    val result = IntArray(2)
+    for (i in nums.indices) {
+        map.put(nums[i], i)
+    }
+    for (i in nums.indices) {
+        val sum = target - nums[i]
+        if (map.containsKey(sum) && map.getValue(sum) != i) {
+            val index = map.getOrDefault(sum, 0)
+            result[0] = i
+            result[1] = index
+        }
+    }
+    return result
+}
